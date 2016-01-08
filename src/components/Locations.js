e7aaa135eea1c76455d98caa279eefa9fc246439
muhammadfaizan <muhammad.faizan01@live.com>
2016-01-08 15:33:05 +0500

    Code bundle reduced by reducing size of this file

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,77 +1,14 @@
+
+
 var React = require('react');
 var AltContainer = require('alt-container');
-var LocationStore = require('../stores/LocationStore');
-var FavoritesStore = require('../stores/FavoritesStore');
-var LocationActions = require('../actions/LocationActions');
-import List from 'material-ui/lib/lists/list';
-import Divider from 'material-ui/lib/divider';
-import ListItem from 'material-ui/lib/lists/list-item';
-import Paper from 'material-ui/lib/paper';
+import LocationStore from '../stores/LocationStore';
+import FavoritesStore from '../stores/FavoritesStore';
+import Favorites from './FavouriteLocation.js';
+import AllLocations from './AllLocation.js';
 
-var Favorites = React.createClass({
 
-  cssStyle:{
-    display: "inline",
-    padding: "10px",
-    margin: "10px"
-  },
-  render() {
-    return (
-        <div>
-      <ul>
-        {this.props.locations.map((location, i) => {
-          return (
-            <li key={i} style={this.cssStyle}><Paper depth={4}>{location.name}</Paper></li>
-          );
-        })}
-      </ul>
-          {this.props.children}
-          </div>
-    );
-  }
-});
 
-var AllLocations = React.createClass({
-  addFave(ev) {
-    var location = LocationStore.getLocation(
-      Number(ev.target.getAttribute('data-id'))
-    );
-    LocationActions.favoriteLocation(location);
-  },
-  render() {
-    if (this.props.errorMessage) {
-      return (
-        <div>{this.props.errorMessage}</div>
-      );
-    }
-
-    if (LocationStore.isLoading()) {
-      return (
-        <div>
-          <img src="ajax-loader.gif" />
-        </div>
-      )
-    }
-
-    return (
-      <ul>
-        {this.props.locations.map((location, i) => {
-          var faveButton = (
-            <button onClick={this.addFave} data-id={location.id}>
-              Favorite
-            </button>
-          );
-
-          return (
-            <li key={i} >
-              {location.name} {location.has_favorite ? '<3' : faveButton}
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
-});
 
 var Locations = React.createClass({
   componentDidMount() {
