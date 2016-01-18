03244c86b8705cd49d2da6b5e7b2d2c0888d08ad
tahir-masood1 <onlytahir@hotmail.com>
2016-01-18 10:54:23 +0500

    map with draw options
    Signed-off-by: tahir-masood1 <onlytahir@hotmail.com>

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,11 +11,17 @@ class Home extends React.Component {
           <Link to="/location">
             <RaisedButton primary={true} label="Location"/>
           </Link>
+          <Link to="/autocomplete">
+            <RaisedButton primary={true} label="AutoComplete"/>
+          </Link>
           <Link to="/map">
             <RaisedButton primary={true} label="Map"/>
           </Link>
           <Link to="/journeys">
             <RaisedButton primary={true} label="Journeys"/>
+          </Link>
+          <Link to="/routemap">
+            <RaisedButton primary={true} label="Directed Map"/>
           </Link>
         </div>
 
