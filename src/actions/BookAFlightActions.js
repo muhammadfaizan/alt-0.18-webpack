fcfbb50d4d154c2b2d3ea9fe4e7f6df571859f49
muhammadfaizan <muhammad.faizan01@live.com>
2016-02-01 16:45:53 +0500

    BAFMap Actions

--- src/actions/BookAFlightActions.js
+++ src/actions/BookAFlightActions.js
@@ -3,3 +3,7 @@
  */
 /* TODO: ADD actions for dispatching it to BookAFlight(BAF) Stores,
    TODO: Identify Actions from Form, it must have actions foreach form fields onChange*/
+
+import alt from '../AltApp';
+
+export default alt.generateActions('newLeg','getLeg', 'getAllLeg');
