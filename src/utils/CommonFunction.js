5d8e23381cfea565e1d686d0d43f22079d0557dd
muhammadfaizan <muhammad.faizan01@live.com>
2016-02-02 15:41:02 +0500

    getOrdinal Function added

--- src/utils/CommonFunction.js
+++ src/utils/CommonFunction.js
@@ -1,3 +1,15 @@
 /**
  * Created by Muhammad Faizan on 02-Feb-16.
  */
+
+/**
+ * @param {number} n - number you want to make ordinal of
+ * @return {string} return - a string that will be ordinal form of parameter
+ */
+module.export = {
+	getOrdinal = n => {
+		var s=["th","st","nd","rd"],
+			v=n%100;
+		return n+( s[ (v-20) % 10 ] || s[v] || s[0] );
+	}
+}
