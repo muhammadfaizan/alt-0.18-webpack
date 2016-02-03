f01036bddde15668306c45c433be11cfa79d18aa
tahir-masood1 <onlytahir@hotmail.com>
2016-02-03 12:26:53 +0500

    update Booking form Signed-off-by: tahir-masood1 <onlytahir@hotmail.com>

--- src/components/BookAFlightForm.js
+++ src/components/BookAFlightForm.js
@@ -3,6 +3,8 @@
  */
 import React from 'react';
 import Slider from 'material-ui/lib/slider';
+import DatePicker from 'material-ui/lib/date-picker/date-picker';
+import TimePicker from 'material-ui/lib/time-picker';
 
 class BookAFlightForm extends React.Component {
   render() {
@@ -11,7 +13,15 @@ class BookAFlightForm extends React.Component {
 	      <form className='flight-form'>
 		      <p className='flight-form-label'>Departure Date</p>
 		      <p className='flight-form-input-container'>
-			      <input name="datetime" className='flight-form-input' type='text' placeholder="Select Date & Time" />
+				<DatePicker
+					id = "myDatepicker"
+					hintText="Inline (AutoOk)"
+					container="inline"
+					autoOk={true}
+					onChange={this.handleChange}/>
+				<TimePicker
+					format="ampm"
+					hintText="12hr Format" />
 		      </p>
 		      <p className='flight-form-label'>From</p>
 		      <p>
